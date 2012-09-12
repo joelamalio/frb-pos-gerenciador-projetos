622ced72130f13e5a822d85ac7c12efcc0fac2f9
antoniojunior87 <>
2012-09-12 19:43:10 -0300

    antoniojunior87

---
+++ GerenciadorProjetos-service-impl/src/main/java/br/edu/frb/gerenciadorprojetos/gerenciadorprojetos/service/business/FuncaoServiceImpl.java
@@ -0,0 +1,27 @@
+package br.edu.frb.gerenciadorprojetos.gerenciadorprojetos.service.business;
+
+import br.edu.frb.gerenciadorprojetos.common.business.FuncaoService;
+import br.edu.frb.gerenciadorprojetos.common.entity.Funcao;
+import br.edu.frb.gerenciadorprojetos.common.entity.Profissional;
+import br.edu.frb.gerenciadorprojetos.gerenciadorprojetos.service.business.dao.GenericDao;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+
+/**
+ * @author antoniojunior87
+ */
+@Stateless
+public class FuncaoServiceImpl implements FuncaoService {
+
+    @EJB
+    GenericDao dao;
+
+    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
+    @Override
+    public List<Funcao> listar() {
+        return dao.listarTodos(Profissional.class);
+    }
+}
