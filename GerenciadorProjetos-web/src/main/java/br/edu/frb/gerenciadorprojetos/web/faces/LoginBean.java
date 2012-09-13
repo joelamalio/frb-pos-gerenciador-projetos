3819378dea3c084c93cec178eea136bd77c7bc33
brunomsc <>
2012-09-12 21:42:45 -0300

    Criar login

--- GerenciadorProjetos-web/src/main/java/br/edu/frb/gerenciadorprojetos/web/faces/LoginBean.java
+++ GerenciadorProjetos-web/src/main/java/br/edu/frb/gerenciadorprojetos/web/faces/LoginBean.java
@@ -4,8 +4,10 @@ import br.edu.frb.gerenciadorprojetos.common.entity.Usuario;
 import br.edu.frb.gerenciadorprojetos.gerenciadorprojetos.service.business.LoginService;
 import java.io.Serializable;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
 /**
  *
@@ -29,8 +31,12 @@ public class LoginBean implements Serializable {
     }
 
     public String logar() {
-        service.logar(usuario);
-        return "logar";
+        Usuario usuarioLogado = service.logar(usuario);
+        if(null != usuarioLogado){
+            return "index";
+        }
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "", "Usuário não existe!"));
+        return "login";
     }
 
     public Usuario getUsuario() {
